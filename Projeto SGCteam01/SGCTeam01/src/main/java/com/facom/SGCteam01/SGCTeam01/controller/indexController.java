2fe4b065eb84d65571b58f069ba8c53f2407cb36
thiago nunes <thiago-nunes-16@hotmail.com>
2017-07-13 20:35:08 -0400

    Classe conferencia

--- Projeto SGCteam01/SGCTeam01/src/main/java/com/facom/SGCteam01/SGCTeam01/controller/indexController.java
+++ Projeto SGCteam01/SGCTeam01/src/main/java/com/facom/SGCteam01/SGCTeam01/controller/indexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class indexController {
 
-	@RequestMapping(value = "/teste")
+	@RequestMapping(value = "/index")
 	public String home() {
 		return "home";
 	}
