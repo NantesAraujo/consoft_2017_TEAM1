0599dced9451a1d013070299e7f2ff7f5d52b6ae
thiago nunes <thiago-nunes-16@hotmail.com>
2017-07-19 19:45:45 -0400

    Teste de persistencia da Classe Conferencia

--- Projeto SGCteam01/SGCTeam01/src/main/java/com/facom/SGCteam01/SGCTeam01/controller/ConferenciaController.java
+++ Projeto SGCteam01/SGCTeam01/src/main/java/com/facom/SGCteam01/SGCTeam01/controller/ConferenciaController.java
@@ -6,14 +6,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ConferenciaController {
 
-	@RequestMapping(value = "/index")
-	public String telaHome() {
+	// @Autowired
+	// private ConferenciaService conferenciaService;
+
+	@RequestMapping("/")
+	public String index() {
 		return "index";
 	}
 
-	@RequestMapping(value = "/confListagem")
-	public String conferenciaListagem() {
-		return "conferenciaListagem";
-	}
-
+	// @RequestMapping(value = "/conferenciaTodos", method = RequestMethod.GET,
+	// produces = MediaType.APPLICATION_JSON_VALUE)
+	// @ResponseBody
+	// public List<Conferencia> teste() {
+	// List<Conferencia> lista = conferenciaService.buscarTodos();
+	//
+	// return lista;
+	// }
+	//
+	// @RequestMapping(value = "/conferenciaNovo", method = RequestMethod.POST)
+	// public String novoTeste() {
+	// Conferencia conferencia = new Conferencia();
+	// conferencia.setNome("conferencia de teste 1");
+	// conferencia.setSubTitulo("conferencia sub titulo 1");
+	// conferencia.setPaginaWeb("www.google.com.br/conferencia");
+	// conferencia.setCidade("Campo Grande");
+	// conferencia.setDataFim(new Date());
+	// conferencia.setDataFim(new Date());
+	// conferencia.setEmailContato("thiago-nunes-16@hotmail.com");
+	//
+	// conferencia = conferenciaService.salvar(conferencia);
+	//
+	// return (conferencia.getId() != null ? "salvo com sucesso" : "");
+	// }
+	//
+	// @RequestMapping(value = "/deletarConferencia", method =
+	// RequestMethod.DELETE)
+	// public String deletar(@RequestParam(value = "id", required = true) Long
+	// id) {
+	// Conferencia conferencia = conferenciaService.buscarPorId(id);
+	//
+	// conferenciaService.deletar(id);
+	//
+	// return "Conferencia de id: " + conferencia.getId() + " removida com
+	// sucesso.";
+	// }
 }
--- Projeto SGCteam01/SGCTeam01/src/main/java/com/facom/SGCteam01/SGCTeam01/model/Conferencia.java
+++ Projeto SGCteam01/SGCTeam01/src/main/java/com/facom/SGCteam01/SGCTeam01/model/Conferencia.java
@@ -1,5 +1,8 @@
 package com.facom.SGCteam01.SGCTeam01.model;
 
+import java.io.Serializable;
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,14 +10,34 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Conferencia {
+public class Conferencia implements Serializable {
+
+	private static final long serialVersionUID = 1230694956112443874L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
 	@Column
-	private String nomeConferencia;
+	private String nome;
+
+	@Column
+	private String subTitulo;
+
+	@Column
+	private String paginaWeb;
+
+	@Column
+	private String cidade;
+
+	@Column
+	private String emailContato;
+
+	@Column
+	private Date dataInicio;
+
+	@Column
+	private Date dataFim;
 
 	public Long getId() {
 		return id;
@@ -24,11 +47,59 @@ public class Conferencia {
 		this.id = id;
 	}
 
-	public String getNomeConferencia() {
-		return nomeConferencia;
+	public String getNome() {
+		return nome;
 	}
 
-	public void setNomeConferencia(String nomeConferencia) {
-		this.nomeConferencia = nomeConferencia;
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSubTitulo() {
+		return subTitulo;
+	}
+
+	public void setSubTitulo(String subTitulo) {
+		this.subTitulo = subTitulo;
+	}
+
+	public String getPaginaWeb() {
+		return paginaWeb;
+	}
+
+	public void setPaginaWeb(String paginaWeb) {
+		this.paginaWeb = paginaWeb;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEmailContato() {
+		return emailContato;
+	}
+
+	public void setEmailContato(String emailContato) {
+		this.emailContato = emailContato;
+	}
+
+	public Date getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(Date dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public Date getDataFim() {
+		return dataFim;
+	}
+
+	public void setDataFim(Date dataFim) {
+		this.dataFim = dataFim;
 	}
 }
