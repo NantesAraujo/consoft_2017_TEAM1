f71908f11cff001a5cbd144a6c2a1acd3bf56d52
thiago nunes <thiago-nunes-16@hotmail.com>
2017-07-19 19:42:40 -0400

    Teste de persistencia da Classe Conferencia

---
+++ Projeto SGCteam01/sgcteam01/src/main/java/com/facom/SGCteam01/SGCTeam01/repository/ConferenciaRepository.java
@@ -0,0 +1,8 @@
+package com.facom.SGCteam01.SGCTeam01.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.facom.SGCteam01.SGCTeam01.model.Conferencia;
+
+public interface ConferenciaRepository extends JpaRepository<Conferencia, Long> {
+}
